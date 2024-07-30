1792423578906065eb8f468940c162cd73de5b84
Shivtej9969 <shivtejbhoslay@gmail.com>
2024-07-30 10:34:33 +0530

    First Commit...

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="Yes";
+		
+		if(hungry=="Yes")
+			System.out.println("eat Something");
+		else {
+			System.out.println("Do work");
+		}
+
+	}
+
+}
